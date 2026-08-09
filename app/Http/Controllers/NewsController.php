b98b6140de522e140d8c9690ec8544dc52692517
ricoi <ricoisdenaam@gmail.com>
2026-08-09 16:29:02 +0200

    Image uploading added

--- app/Http/Controllers/NewsController.php
+++ app/Http/Controllers/NewsController.php
@@ -7,18 +7,31 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Illuminate\Support\Facades\Storage;
 use Inertia\Inertia;
+
+use Intervention\Image\Drivers\Imagick\Driver;
 use Intervention\Image\Image;
+use Intervention\Image\ImageManager;
 use Str;
 
 class NewsController extends Controller
 {
     public function index()
     {
+        $news = News::query()->where('status', 1)->where('published', true)->latest()->paginate(10)
+        ->withQueryString();
+
+        foreach ($news as $newsArticle) {
+            if ($newsArticle->image === "images/header.jpg") {
+                continue;
+            }
+
+            $newsArticle->image = Storage::disk('public')->url($newsArticle->image);
+        }
+
         return Inertia::render('News/Index', [
             'canLogin' => (Route::has('login') && ! auth()->check()),
             'canRegister' => Route::has('register'),
-            'news' => News::query()->where('status', 1)->where('published', true)->latest()->paginate(10)
-                ->withQueryString(),
+            'news' => $news
         ]);
 
     }
@@ -49,8 +62,10 @@ class NewsController extends Controller
         $data['slug'] = str_replace(' ', '-', $data['title']);
 
         if ($request->hasFile('image')) {
+            $manager = new ImageManager(new \Intervention\Image\Drivers\Gd\Driver());
+
             // if image is present, we clean it before storing it and keep the returned path
-            $encoded = Image::read($request->file('image'))
+            $encoded = $manager->read($request->file('image'))
                 ->cover(400, 400)
                 ->toWebp(80);
 
